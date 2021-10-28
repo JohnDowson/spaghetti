59de5fb186edb1a75024f9d3f468eb3c83ec065e
Ivan Chinenov <ichinenov@gmail.com>
2021-10-28 07:06:10 +0300

    Dependencies

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@ docker build --add-host=host.docker.internal:host-gateway -t spaghetti . &&\
 docker run --name spaghetti1 spaghetti &&\
 docker cp spaghetti1:/spaghetti/target/release/spaghetti ./spaghetti.bin &&\
 docker rm spaghetti1
+strip spaghetti.bin
